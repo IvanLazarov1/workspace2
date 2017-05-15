d2231afce0732a06397c06d46d72bd9df31b70c3
IvanLazarov1 <vrx@abv.bg>
2017-05-15 13:00:27 +0300

    15.05.2017

---
+++ Triangle1/Triangle1.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : Triangle1.cpp
+// Author      : ty
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Triangle.h"
+using namespace std;
+
+int main() {
+
+	Triangle a,b;
+
+	cin >> a;
+	cin >> b;
+
+	if(a == b){
+		cout << "equals";
+	}else{
+
+		cout << "not equals";
+	}
+
+
+
+
+
+	return 0;
+}
