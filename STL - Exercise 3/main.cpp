35ff1cbae28ef759bdc838b326ce9f8cabfabd99
IvanLazarov1 <vrx@abv.bg>
2017-05-31 20:24:25 +0300

    STL - SDL

---
+++ STL - Exercise 3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	cout << "0 (Zero) Terminates:" << endl;
+	stack<int> integers;
+	int number = 1;
+
+	while (number != 0)
+	{
+		if (number < 0)
+		{
+			cerr << "Error! Number is below Zero (0)." << endl;
+			break;
+		}
+
+		cout << "Enter Number: ";
+		cin >> number;
+
+		if (number == 0)
+		{
+			break;
+		}
+
+		integers.push(number);
+	}
+
+	cout << "Numbers backwards printed are: "<<endl;
+
+	while (!integers.empty())
+	{
+       cout << integers.top() << " ";
+       integers.pop();
+	}
+
+	return 0;
+}
